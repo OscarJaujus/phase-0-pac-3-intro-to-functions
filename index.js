0bd86efbbfd8b672db897abc0aca7552b80113ce
Oscar Omondi <oscaromondiomondi@gmail.com>
2022-06-09 14:22:13 +0300

    reviewing my notes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Follow along with the examples here
+// function doNothing() {
+// }
+
+// function sayHello() {
+//    console.log('Hello');
+//}
+// sayHello();
+
+// WHILE Can be used to make password locks
+// let i = 0 // this logs Thank you 10 times
+// while (i < 10) {
+//    console.log('Thank You')
+//    i++
+// }
+
+
+let x = 0
+while (x < 3) {
+    console.log('Invalid')
+    x++
+}
+// this logs INVALID 3 times
+
